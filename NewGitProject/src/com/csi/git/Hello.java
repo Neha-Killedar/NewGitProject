c86af6e1e02bdeb6baf9d51b0fd7125dbe0775dd
Neha Killedar <56631922+Neha-Killedar@users.noreply.github.com>
2019-11-20 09:41:32 +0530

    Update Hello.java

--- NewGitProject/src/com/csi/git/Hello.java
+++ NewGitProject/src/com/csi/git/Hello.java
@@ -39,7 +39,7 @@ public String findDuplicate (String input)throws NoSuchFieldException, SecurityE
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		System.out.println("Working properly");
 	}
 	}
 
