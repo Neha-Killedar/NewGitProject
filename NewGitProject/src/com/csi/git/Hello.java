9450fafd85b8ffb8d5a8f44094141ba7c885cf06
NEHA <NEHA@192.168.1.207>
2019-11-20 09:36:12 +0530

    Duplicate charater program added

---
+++ NewGitProject/src/com/csi/git/Hello.java
@@ -0,0 +1,45 @@
+package com.csi.git;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hello {
+public String findDuplicate (String input)throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		
+		Field valueField= String.class.getDeclaredField("value");
+		valueField.setAccessible(true);
+		char[] chars= (char[]) valueField.get(input);
+		List<Character> duplicateCharater= new ArrayList<Character>();
+		for(int i= 0; i < chars.length; i++){
+			char achar=chars[i];
+			for(int j=i+1; j < chars.length; j++){
+				char anotherChar=chars[j];
+				if(achar==anotherChar){
+					if(duplicateCharater.contains(achar)){
+						duplicateCharater.add(achar);
+					}
+				}
+				
+			}
+		}
+		
+		char[] charArray= new char[duplicateCharater.size()];
+		for(int i=0; i < duplicateCharater.size(); i++){
+			charArray[i]=duplicateCharater.get(i);
+			
+		}
+		return new String(charArray);
+	
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	}
+
+
